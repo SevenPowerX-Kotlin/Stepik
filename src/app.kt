5b9eae7521cf013d8cfd91ac2f7f22ae45afbe70
splaa_Mac3.1 <splaandrey@gmail.com>
2019-06-27 10:56:26 +0200

    + Added app.kt "Hello Kotlin"

---
+++ src/app.kt
@@ -0,0 +1,10 @@
+/**
+ * Первая программа на Kotlin в IntelliJ IDEA
+ * https://metanit.com/kotlin/tutorial/1.2.php
+ */
+
+fun main(args: Array<String>) {
+    println("Hello Kotlin")
+}
+
+//todo-splaa: Перейти к Глава 2.Основы языка Kotlin. Переменные https://metanit.com/kotlin/tutorial/2.1.php
